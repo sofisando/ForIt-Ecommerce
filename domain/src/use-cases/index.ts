80e20e3f44a4ae7f22212d88c2622aa2199d465e
sofisando <sofisandobal9@gmail.com>
2025-11-24 20:09:14 -0300

    update index barrels

--- domain/src/use-cases/index.ts
+++ domain/src/use-cases/index.ts
@@ -3,5 +3,6 @@ export * from "./category/index";
 export * from "./discount/index";
 export * from "./order/index";
 export * from "./product/index";
+export * from "./stock/index";
 export * from "./user/index";
 export * from "./variant/index";
---
+++ domain/src/use-cases/stock/index.ts
@@ -0,0 +1,7 @@
+export * from './create-stock';
+export * from './decrease-stock';
+export * from './delete-stock';
+export * from './edit-quantity-stock';
+export * from './get-stock-by-variant';
+export * from './get-stock-list';
+export * from './increase-stock';
