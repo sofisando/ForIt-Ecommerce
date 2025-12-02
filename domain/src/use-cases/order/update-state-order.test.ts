a43887aebab0da9bb986a93af401458851bac4fd
sofisando <sofisandobal9@gmail.com>
2025-12-02 02:13:16 -0300

    add verify user Admin and notification in update-state-order

--- domain/src/use-cases/order/update-state-order.test.ts
+++ domain/src/use-cases/order/update-state-order.test.ts
@@ -3,6 +3,9 @@ import { MockedOrderService } from "../../services/mocks/mock-order-service";
 import { orderMock } from "../../entities/mocks/order-mock";
 import { updateStateOrder } from "./update-state-order";
 import { Order } from "../../entities";
+import { MockedUserService } from "../../services/mocks/mock-user-service";
+import { userMock } from "../../entities/mocks/user-mock";
+import { MockedEmailService } from "../../services/mocks/mock-email-service";
 
 describe("updateStateOrder", async () => {
   const orderService = new MockedOrderService([
@@ -11,20 +14,45 @@ describe("updateStateOrder", async () => {
       state: "pending",
     }),
   ]);
+  const userService = new MockedUserService([userMock({ id: "user-1", email: "prueba@gmail.com" ,role: "ADMIN"}),
+    userMock({ id: "user-2", role: "CLIENT"})
+  ])
+  const emailService = new MockedEmailService();
 
-  test("When change state in order you should update state in order", async () => {
+  test("SUCCESS CASE - if user found and is ADMIN, should change state in order", async () => {
     const result = (await updateStateOrder(
-      { orderService },
-      { id: "order-1", state: "accepted" }
+      { orderService, userService, emailService },
+      { orderId: "order-1", userId: "user-1", state: "accepted" }
     )) as Order;
 
     expect(result.state).toStrictEqual("accepted");
+    expect(emailService.sent.length).toBe(1);
+    expect(emailService.sent[0]).toMatchObject({
+      to: "prueba@gmail.com",
+      subject: "Actualización del estado de tu orden",
+    });
+  });
+
+  test("Should return error if user not found", async () => {
+    const result = await updateStateOrder(
+      { orderService, userService, emailService },
+      { orderId: "order-1", userId: "user-999" , state: "accepted" }
+    );
+    expect(result).toStrictEqual(Error("User not found"));
+  });
+
+  test("Should return error if user is not ADMIN", async () => {
+    const result = await updateStateOrder(
+      { orderService, userService, emailService },
+      { orderId: "order-1", userId: "user-2" , state: "accepted" }
+    );
+    expect(result).toStrictEqual(Error("User is not ADMIN"));
   });
 
   test("Should return error if order not found", async () => {
     const result = await updateStateOrder(
-      { orderService },
-      { id: "order-999", state: "accepted" }
+      { orderService, userService, emailService },
+      { orderId: "order-999", userId: "user-1" , state: "accepted" }
     );
     expect(result).toStrictEqual(Error("Order not found"));
   });
--- domain/src/use-cases/order/update-state-order.ts
+++ domain/src/use-cases/order/update-state-order.ts
@@ -1,24 +1,43 @@
+import { User, UserRole } from "../../entities";
 import { Order, OrderState } from "../../entities/order";
-import { OrderService } from "../../services/order-service";
+import type { EmailService, OrderService, UserService } from "../../services";
+import { notifyUpdateStateOrder } from "../email/notify-update-state-order";
 
 interface UpdateStateOrderDeps {
   orderService: OrderService;
+  emailService: EmailService;
+  userService: UserService;
 }
 
 interface UpdateStateOrderPayload {
-  id: Order["id"];
-  state: OrderState;
+  orderId: Order["id"];
+  userId: User["id"];
+  state: Exclude<OrderState, "pending" | "canceled">;
 }
 
 export async function updateStateOrder(
-  { orderService }: UpdateStateOrderDeps,
-  { id, state }: UpdateStateOrderPayload
+  { orderService, userService, emailService }: UpdateStateOrderDeps,
+  { orderId, userId, state }: UpdateStateOrderPayload
 ): Promise<Order | Error> {
-  const findedOrder = await orderService.findById(id);
+
+  const user = await userService.findById(userId);
+  if (!user) return new Error("User not found");
+  
+  if (user.role !== UserRole.ADMIN) {
+    return new Error(`User is not ${UserRole.ADMIN}`);
+  }
+  const findedOrder = await orderService.findById(orderId);
   if (!findedOrder){
     return new Error("Order not found");
   }
 
-  const updatedOrder = await orderService.updateStateOrder(id, state);
+  const updatedOrder = await orderService.updateStateOrder(orderId, state);
+
+  // Mandar notificacion por email
+    await notifyUpdateStateOrder(
+      { emailService, orderService },
+      { userEmail: user.email, orderId: findedOrder.id, state: state }
+    );
+
   return updatedOrder;
 }
