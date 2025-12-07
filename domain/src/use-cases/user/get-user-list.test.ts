23cdd61510bf88995aaaad5ea729df956517cc89
sofisando <sofisandobal9@gmail.com>
2025-12-07 16:53:30 -0300

    add verification admin user in get-user-list use case and test

--- domain/src/use-cases/user/get-user-list.test.ts
+++ domain/src/use-cases/user/get-user-list.test.ts
@@ -1,26 +1,28 @@
-import { describe, test, expect} from "vitest";
+import { describe, test, expect } from "vitest";
 import { MockedUserService } from "../../services/mocks/mock-user-service";
 import { userMock } from "../../entities/mocks/user-mock";
 import { getUserList } from "./get-user-list";
 
-describe("getUserList", async() => {
-    test("Should return a array of users", async () => {
-        const userService = new MockedUserService([
-            userMock(),
-            userMock()
-        ]) //acá ejecutamos el constructor del Mock de servicio y le metemos Mocks de usuario
-        const result = await getUserList(
-            { userService }
-        );
-        expect(result).toHaveLength(2);
-        expect(result).toStrictEqual(userService.users);
-    });
-    test("if there are no users you should return an empty list", async () => {;
-        const userService = new MockedUserService([])
-        const result = await getUserList(
-            { userService }
-        );
-        expect(result).toHaveLength(0);
-        expect(result).toStrictEqual([]);
-    });
+describe("getUserList", async () => {
+  const userService = new MockedUserService([
+    userMock({ id: "user-1", role: "ADMIN" }),
+    userMock({ id: "user-2", role: "CLIENT" }),
+    userMock(),
+  ]);
+
+  test("Should return a array of users", async () => {
+    const result = await getUserList({ userService }, { userId: "user-1" });
+    expect(result).toHaveLength(3);
+    expect(result).toStrictEqual(userService.users);
+  });
+  
+  test("Should return error if user not found", async () => {
+    const result = await getUserList({ userService }, { userId: "user-999" });
+    expect(result).toStrictEqual(Error("User user-999 not found"));
+  });
+
+  test("Should return error if user is not ADMIN", async () => {
+    const result = await getUserList({ userService }, { userId: "user-2" });
+    expect(result).toStrictEqual(Error("User is not ADMIN"));
+  });
 });
--- domain/src/use-cases/user/get-user-list.ts
+++ domain/src/use-cases/user/get-user-list.ts
@@ -1,11 +1,23 @@
+import { User, UserRole } from "../../entities";
 import { UserService } from "../../services";
 
 interface GetUserListDeps {
   userService: UserService;
 }
-//no tiene payload porque no necesita nada para traer la lista de usuarios
+interface GetUserListPayload {
+  userId: User["id"];
+}
 
-export async function getUserList({ userService }: GetUserListDeps) {
+export async function getUserList(
+  { userService }: GetUserListDeps,
+  { userId }: GetUserListPayload
+): Promise<User[] | Error> {
+  const user = await userService.findById(userId);
+  if (!user) return new Error(`User ${userId} not found`);
+
+  if (user.role !== UserRole.ADMIN) {
+    return new Error(`User is not ${UserRole.ADMIN}`);
+  }
   const users = await userService.findAll();
   return users;
 }
