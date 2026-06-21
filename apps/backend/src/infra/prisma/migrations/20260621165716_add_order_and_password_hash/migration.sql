7620f9a492305caafcd98141ba59ffc84dbc1b8f
sofisando <sofisandobal9@gmail.com>
2026-06-21 14:06:53 -0300

    prisma migration addOrder_and_passwordHash and remove comments from product-repo

---
+++ apps/backend/src/infra/prisma/migrations/20260621165716_add_order_and_password_hash/migration.sql
@@ -0,0 +1,72 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `branchId` on the `Order` table. All the data in the column will be lost.
+  - You are about to alter the column `total` on the `Order` table. The data in that column could be lost. The data in that column will be cast from `Decimal(65,30)` to `Decimal(10,2)`.
+  - You are about to drop the column `discount` on the `OrderItem` table. All the data in the column will be lost.
+  - You are about to drop the column `name` on the `OrderItem` table. All the data in the column will be lost.
+  - You are about to drop the column `price` on the `OrderItem` table. All the data in the column will be lost.
+  - You are about to drop the column `productId` on the `OrderItem` table. All the data in the column will be lost.
+  - You are about to drop the column `subtotal` on the `OrderItem` table. All the data in the column will be lost.
+  - You are about to drop the column `variant` on the `OrderItem` table. All the data in the column will be lost.
+  - You are about to drop the column `password` on the `User` table. All the data in the column will be lost.
+  - Added the required column `snapshot` to the `OrderItem` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `unitPrice` to the `OrderItem` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `passwordHash` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Order" DROP COLUMN "branchId",
+ALTER COLUMN "total" SET DATA TYPE DECIMAL(10,2);
+
+-- AlterTable
+ALTER TABLE "OrderItem" DROP COLUMN "discount",
+DROP COLUMN "name",
+DROP COLUMN "price",
+DROP COLUMN "productId",
+DROP COLUMN "subtotal",
+DROP COLUMN "variant",
+ADD COLUMN     "snapshot" JSONB NOT NULL,
+ADD COLUMN     "unitPrice" DECIMAL(10,2) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User"
+RENAME COLUMN "password" TO "passwordHash";
+
+-- CreateTable
+CREATE TABLE "Cart" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Cart_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "CartItem" (
+    "id" TEXT NOT NULL,
+    "cartId" TEXT NOT NULL,
+    "productId" TEXT NOT NULL,
+    "quantity" INTEGER NOT NULL,
+
+    CONSTRAINT "CartItem_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Cart_userId_key" ON "Cart"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "CartItem_cartId_productId_key" ON "CartItem"("cartId", "productId");
+
+-- AddForeignKey
+ALTER TABLE "Cart" ADD CONSTRAINT "Cart_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CartItem" ADD CONSTRAINT "CartItem_cartId_fkey" FOREIGN KEY ("cartId") REFERENCES "Cart"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "CartItem" ADD CONSTRAINT "CartItem_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
